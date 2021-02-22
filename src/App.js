b2dfe2c93f10041ed7e800aad6fe939febec480e
Rajbir02 <67579379+Rajbir02@users.noreply.github.com>
2021-02-22 20:59:48 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
-import { Container, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import {
+  Container, Jumbotron, Button,
+  Navbar, Nav, Form, FormControl
+} from 'react-bootstrap';
+
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 import Home from './Home'
 import About from './About'
+import Pricing from './Pricing'
+import Register from './Register'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename="flower-app">
       <Container>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="flowerapp/">Home</Nav.Link>
-              <Nav.Link href="flowerapp/about">About Us</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href={`/`}>
+            <img
+              src={`${process.env.PUBLIC_URL}/flower.png`}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+            /> Flower
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/pricing">Pricing</Link>
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
         </Navbar>
+
         <Switch>
-          <Route exact path='flowerapp/' component={Home} />
-          <Route exact path='flowerapp/about' component={About} />
-          <Route render={function () {
-            return <p>Not found</p>
-          }} />
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/pricing">
+            <Pricing />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
+
+
+
       </Container>
-    </BrowserRouter>
+    </Router>
   );
 }
 
